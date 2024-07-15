f8b6f9c5f04a753ded4e77e88b89bcdabdfdb1b7
HarshitPatel32 <121165660+HarshitPatel32@users.noreply.github.com>
2024-07-15 18:41:11 +0530

    Update _action.ts

--- app/actions/_action.ts
+++ app/actions/_action.ts
@@ -13,16 +13,13 @@ export async function saveFile(formData: FormData): Promise<void> {
   const data = await file.arrayBuffer();
   const tempFolderPath = path.join(process.cwd(), 'uploads');
 
-  // Ensure the tempfolder exists
   await fs.mkdir(tempFolderPath, { recursive: true });
 
-  // Get the current date and time in a shorter format
   const now = new Date();
   const formattedDate = `${now.getFullYear()}${(now.getMonth() + 1).toString().padStart(2, '0')}${now.getDate().toString().padStart(2, '0')}`;
   const formattedTime = `${now.getHours().toString().padStart(2, '0')}${now.getMinutes().toString().padStart(2, '0')}${now.getSeconds().toString().padStart(2, '0')}`;
   const currentDateTime = `${formattedDate}_${formattedTime}`;
 
-  // Create the new file name
   const originalFileName = file.name;
   const fileExtension = path.extname(originalFileName);
   const baseFileName = path.basename(originalFileName, fileExtension).split(' ')[0].replace(/ /g, '_');
